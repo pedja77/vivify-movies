bb3958b9fba793f31f64531b0382a4095843fbe3
pedja77 <pedja77@stcable.net>
2018-04-11 20:05:25 +0200

    Basic routing

---
+++ movies/resources/views/movies/movies-index.blade.php
@@ -0,0 +1,8 @@
+<ul>
+@foreach($movies as $movie)
+    <li>
+        <h4><a href="{{ route('single-movie', ['id'=> $movie->id]) }}">{{ $movie->title }}</a></h4>
+        <p>{{  substr($movie->storyline, 0, 35) . '...' }}
+    </li>
+@endforeach
+</ul>
---
+++ movies/resources/views/movies/movies-show.blade.php
@@ -0,0 +1,5 @@
+<h3>{{ $movie->title  }}</h3>
+Director: {{ $movie->director }}
+Year: {{ $movie->year }}
+Story:
+<p>{{ $movie->storyline }}</p>
